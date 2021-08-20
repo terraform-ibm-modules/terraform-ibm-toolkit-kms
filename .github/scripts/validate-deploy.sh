0dde92b852693f2aeedeae28eb1948a31a3fcdf6
Sean Sundberg <seansund@us.ibm.com>
2021-08-20 07:29:28 -0500

    Updates validate-deploy logic to check for provisioned service
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -2,4 +2,17 @@
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 
-export KUBECONFIG="${SCRIPT_DIR}/.kube/config"
+NAME=$(cat .resourcename)
+ID=$(cat .resourceid)
+SERVICE=$(cat .resourceservice)
+INPUT_SERVICE=$(cat .inputservice)
+
+if [[ -z "${NAME}" ]] || [[ -z "${ID}" ]]; then
+  echo "Resource values are missing. Nothing provisioned"
+  exit 1
+fi
+
+if [[ "${INPUT_SERVICE}" == "keyprotect" ]] && [[ "${SERVICE}" != "kms" ]] || [[ "${INPUT_SERVICE}" == "hpcs" ]] && [[ "${SERVICE}" != "hs-crypto" ]]; then
+  echo "Provisioned service does not match requested: ${SERVICE}, ${INPUT_SERVICE}"
+  exit 1
+fi
--- test/stages/stage2-kms.tf
+++ test/stages/stage2-kms.tf
@@ -1,10 +1,25 @@
 module "kms" {
   source = "./module"
 
-  service             = "keyprotect"
+  service             = var.service
   resource_group_name = module.resource_group.name
   region              = var.region
   name_prefix         = var.name_prefix
   provision           = true
   ibmcloud_api_key    = var.ibmcloud_api_key
+}
+
+resource null_resource print_names {
+  provisioner "local-exec" {
+    command = "echo -n '${module.kms.name}' > .resourcename"
+  }
+  provisioner "local-exec" {
+    command = "echo -n '${module.kms.id}' > .resourceid"
+  }
+  provisioner "local-exec" {
+    command = "echo -n '${module.kms.service}' > .resourceservice"
+  }
+  provisioner "local-exec" {
+    command = "echo -n '${var.service}' > .inputservice"
+  }
 }
